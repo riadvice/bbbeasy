c7d99cadd9dd1cb4930c93f2671a20d12b5e9399
Saoussen Boulakbech <saoussen.boulakbech@riadvice.tn>
2022-01-18 09:15:07 +0100

    Merge authentication branches

--- hivelvet-frontend/src/components/Login.tsx
+++ hivelvet-frontend/src/components/Login.tsx
@@ -16,94 +16,129 @@
  * with Hivelvet; if not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from "react";
+import { Component } from "react";
 import {Link} from "react-router-dom";
-import * as Yup from "yup";
-import {useForm} from "react-hook-form";
-import {yupResolver} from "@hookform/resolvers/yup/dist/yup";
-import axios from "axios";
 
-function Login() {
-    const validationSchema = Yup.object().shape({
-        email: Yup.string()
-            .required('Email is required')
-            .email('Email is invalid'),
-        password: Yup.string()
-            .required('Password is required')
-            .min(4, 'Password must be at least 4 characters')
-    });
-    const { register, handleSubmit, reset, formState: { errors } } = useForm({ resolver: yupResolver(validationSchema) });
+import AuthService from "../services/auth.service";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from 'yup';
 
-    const addReturnBlock = (message : string, typeMessage : string) => {
-        var responseElement = document.getElementById('responseServer');
-        responseElement?.remove();
+type Props = {};
 
-        var formElement = document.getElementById('formulaire');
-        var divElement = document.createElement('div');
-        divElement.id = 'responseServer';
-        divElement.className = typeMessage == 'error' ? 'alert alert-danger' : 'alert alert-success';
-        divElement.innerHTML = message;
-        formElement?.parentNode?.insertBefore(divElement,formElement);
+type State = {
+    email?: string;
+    password?: string;
+    successful: boolean;
+    message: string;
+};
+
+class Login extends Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.state = {
+            email: '',
+            password: '',
+            successful: false,
+            message: ''
+        };
     }
 
-    const onSubmit = (data : any) => {
-        let url = 'http://hivelvet.test/account/api/login';
-        axios.post(url,data)
+    validationSchema() {
+        return Yup.object().shape({
+            email: Yup.string()
+                .required('Email is required')
+                .email('Email is invalid'),
+            password: Yup.string()
+                .required('Password is required')
+                .min(4, 'Password must be at least 4 characters')
+        });
+    }
+
+    handleLogin(formValue: { email: string; password: string }) {
+        const { email, password } = formValue;
+        AuthService.login(email, password)
             .then(response => {
                 const responseMessage = response.data.message;
+                this.setState({
+                    successful: true,
+                    message: responseMessage
+                });
                 const user = response.data.user;
-                addReturnBlock(responseMessage,'success');
                 localStorage.setItem("user", JSON.stringify(user));
             })
             .catch(error => {
                 const responseMessage = error.response.data.message;
-                addReturnBlock(responseMessage,'error');
+                this.setState({
+                    successful: false,
+                    message: responseMessage
+                });
             });
-    };
-    return (
-        <section className="pricing section">
-            <div className="container-sm">
-                <div className="pricing-inner section-inner section-top">
-                    <div className="pricing-header text-center">
-                        <h2 className="section-title mt-0">Get started</h2>
-                        <p className="section-paragraph mb-0">Sign in to continue to our application </p>
-                    </div>
-                    <div className="pricing-tables-wrap">
-                        <div className="pricing-table page-login">
-                            <div className="pricing-table-inner is-revealing">
-                                <div className="pricing-table-main">
-                                    <form id="formulaire" className="formulaire" method="post" onSubmit={handleSubmit(onSubmit)}>
-                                        <div className="form-group">
-                                            <label className="form-label" htmlFor="user_email">Email</label>
-                                            <input type="text" id="user_email"
-                                                   {...register('email')}
-                                                   className={`form-control ${errors.email ? 'is-invalid' : ''}`}/>
-                                            <div className="invalid-feedback">{errors.email?.message}</div>
+    }
+
+    render() {
+        const { successful, message } = this.state;
+        const initialValues = {
+            email: '',
+            password: '',
+            successful: false,
+            message: ''
+        };
+        return (
+            <section className="pricing section">
+                <div className="container-sm">
+                    <div className="pricing-inner section-inner section-top">
+                        <div className="pricing-header text-center">
+                            <h2 className="section-title mt-0">Get started</h2>
+                            <p className="section-paragraph mb-0">Sign in to continue to our application </p>
+                        </div>
+                        <div className="pricing-tables-wrap">
+                            <div className="pricing-table page-login">
+                                <div className="pricing-table-inner is-revealing">
+                                    <div className="pricing-table-main">
+                                        { message &&
+                                            <div className={ successful ? 'alert alert-success' : 'alert alert-danger'}>
+                                                { message }
+                                            </div>
+                                        }
+                                        <Formik
+                                            initialValues={initialValues}
+                                            validationSchema={this.validationSchema}
+                                            onSubmit={this.handleLogin}>
+                                            {({ errors, touched }) => (
+                                                <Form>
+                                                    <div className="form-group">
+                                                        <label className="form-label" htmlFor="user_email">Email</label>
+                                                        <Field type="email" name="email" id="user_email" className={`form-control ${errors.email && touched.email ? 'is-invalid' : ''}`}/>
+                                                        <ErrorMessage name="email" component="div" className="invalid-feedback"/>
+                                                    </div>
+                                                    <div className="form-group">
+                                                        <label className="form-label" htmlFor="user_password">Password</label>
+                                                        <Field type="password" name="password" id="user_password" className={`form-control ${errors.password && touched.password ? 'is-invalid' : ''}`}/>
+                                                        <ErrorMessage name="password" component="div" className="invalid-feedback"/>
+                                                    </div>
+                                                    <div className="pricing-table-cta mb-8">
+                                                        <button
+                                                            className="button button-primary button-shadow button-block submit-btn">
+                                                            Login now
+                                                        </button>
+                                                    </div>
+                                                </Form>
+                                            )}
+                                        </Formik>
+                                        <div className="text-center mt-24">
+                                            <span className="text-xs">Dont't have an account ? <Link to={'/register'} className="login-link">Register here</Link></span>
                                         </div>
-                                        <div className="form-group">
-                                            <label className="form-label" htmlFor="user_password">Password</label>
-                                            <input type="password" id="user_password"
-                                                   {...register('password')}
-                                                   className={`form-control ${errors.password ? 'is-invalid' : ''}`}/>
-                                            <div className="invalid-feedback">{errors.password?.message}</div>
-                                        </div>
-                                        <div className="pricing-table-cta mb-8">
-                                            <button className="button button-primary button-shadow button-block submit-btn">
-                                                Login now
-                                            </button>
-                                        </div>
-                                    </form>
-                                    <div className="text-center mt-24">
-                                        <span className="text-xs">Dont't have an account ? <Link to={'/register'} className="login-link">Register here</Link></span>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }
 }
 
 export default Login;
--- hivelvet-frontend/src/components/Register.tsx
+++ hivelvet-frontend/src/components/Register.tsx
@@ -16,112 +16,146 @@
  * with Hivelvet; if not, see <http://www.gnu.org/licenses/>.
  */
 
-import React from "react";
+import { Component } from "react";
 import {Link} from "react-router-dom";
 
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+import AuthService from "../services/auth.service";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as Yup from 'yup';
-import axios from 'axios';
 
-const Register = () => {
-    const validationSchema = Yup.object().shape({
-        username: Yup.string()
-            .required('Username is required'),
-        email: Yup.string()
-            .required('Email is required')
-            .email('Email is invalid'),
-        password: Yup.string()
-            .required('Password is required')
-            .min(4, 'Password must be at least 4 characters'),
-        confirmPassword: Yup.string()
-            .required('Confirm password is required')
-            .min(4, 'Password must be at least 4 characters')
-            .oneOf([Yup.ref('password'), null], 'Confirm password does not match')
-    });
-    const { register, handleSubmit, reset, formState: { errors } } = useForm({ resolver: yupResolver(validationSchema) });
+type Props = {};
 
-    const addReturnBlock = (message : string, typeMessage : string) => {
-        var responseElement = document.getElementById('responseServer');
-        responseElement?.remove();
+type State = {
+    username?: string;
+    email?: string;
+    password?: string;
+    confirmPassword?: string;
+    successful: boolean;
+    message: string;
+};
 
-        var formElement = document.getElementById('formulaire');
-        var divElement = document.createElement('div');
-        divElement.id = 'responseServer';
-        divElement.className = typeMessage == 'error' ? 'alert alert-danger' : 'alert alert-success';
-        divElement.innerHTML = message;
-        formElement?.parentNode?.insertBefore(divElement,formElement);
+class Register extends Component<Props, State> {
+
+    constructor(props: Props) {
+        super(props);
+        this.handleRegistration = this.handleRegistration.bind(this);
+        this.state = {
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+            successful: false,
+            message: ''
+        };
     }
 
-    const onSubmit = (data : any) => {
-        let url = 'http://hivelvet.test/account/api/register';
-        axios.post(url,data)
+    validationSchema() {
+        return Yup.object().shape({
+            username: Yup.string()
+                .required('Username is required'),
+            email: Yup.string()
+                .required('Email is required')
+                .email('Email is invalid'),
+            password: Yup.string()
+                .required('Password is required')
+                .min(4, 'Password must be at least 4 characters'),
+            confirmPassword: Yup.string()
+                .required('Confirm password is required')
+                .min(4, 'Password must be at least 4 characters')
+                .oneOf([Yup.ref('password'), null], 'Confirm password does not match')
+        });
+    }
+
+    handleRegistration(formValue: { username: string; email: string; password: string; confirmPassword: string }) {
+        const { username, email, password, confirmPassword } = formValue;
+        AuthService.register(username, email, password, confirmPassword)
             .then(response => {
                 const responseMessage = response.data.message;
-                addReturnBlock(responseMessage,'success');
+                this.setState({
+                    successful: true,
+                    message: responseMessage
+                });
             })
             .catch(error => {
                 const responseMessage = error.response.data.message;
-                addReturnBlock(responseMessage,'error');
+                this.setState({
+                    successful: false,
+                    message: responseMessage
+                });
             });
-    };
-    return (
-        <section className="pricing section">
-            <div className="container-sm">
-                <div className="pricing-inner section-inner section-top">
-                    <div className="pricing-header text-center">
-                        <h2 className="section-title mt-0">Join us</h2>
-                        <p className="section-paragraph mb-0">Register now and join our community</p>
-                    </div>
-                    <div className="pricing-tables-wrap">
-                        <div className="pricing-table">
-                            <div className="pricing-table-inner is-revealing">
-                                <div className="pricing-table-main">
-                                    <form id="formulaire" className="formulaire" method="post" onSubmit={handleSubmit(onSubmit)}>
-                                        <div className="form-group">
-                                            <label className="form-label" htmlFor="user_name">Username</label>
-                                            <input type="text" id="user_name"
-                                                   {...register('username')}
-                                                   className={`form-control ${errors.username ? 'is-invalid' : ''}`}/>
-                                            <div className="invalid-feedback">{errors.username?.message}</div>
+    }
+
+    render() {
+        const { successful, message } = this.state;
+        const initialValues = {
+            username: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+            successful: false,
+            message: ''
+        };
+        return (
+            <section className="pricing section">
+                <div className="container-sm">
+                    <div className="pricing-inner section-inner section-top">
+                        <div className="pricing-header text-center">
+                            <h2 className="section-title mt-0">Join us</h2>
+                            <p className="section-paragraph mb-0">Register now and join our community</p>
+                        </div>
+                        <div className="pricing-tables-wrap">
+                            <div className="pricing-table">
+                                <div className="pricing-table-inner is-revealing">
+                                    <div className="pricing-table-main">
+                                        { message &&
+                                            <div className={ successful ? 'alert alert-success' : 'alert alert-danger'}>
+                                                { message }
+                                            </div>
+                                        }
+                                        <Formik
+                                            initialValues={initialValues}
+                                            validationSchema={this.validationSchema}
+                                            onSubmit={this.handleRegistration} >
+                                            {({ errors, touched }) => (
+                                            <Form>
+                                                <div className="form-group">
+                                                    <label className="form-label" htmlFor="user_name">Username</label>
+                                                    <Field type="text" name="username" id="user_name" className={`form-control ${errors.username && touched.username ? 'is-invalid' : ''}`}/>
+                                                    <ErrorMessage name="username" component="div" className="invalid-feedback"/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label className="form-label" htmlFor="user_email">Email</label>
+                                                    <Field type="email" name="email" id="user_email" className={`form-control ${errors.email && touched.email ? 'is-invalid' : ''}`}/>
+                                                    <ErrorMessage name="email" component="div" className="invalid-feedback"/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label className="form-label" htmlFor="user_password">Password</label>
+                                                    <Field type="password" name="password" id="user_password" className={`form-control ${errors.password && touched.password ? 'is-invalid' : ''}`}/>
+                                                    <ErrorMessage name="password" component="div" className="invalid-feedback"/>
+                                                </div>
+                                                <div className="form-group">
+                                                    <label className="form-label" htmlFor="user_password_confirm">Confirm password</label>
+                                                    <Field type="password" name="confirmPassword" id="user_password_confirm" className={`form-control ${errors.confirmPassword && touched.confirmPassword ? 'is-invalid' : ''}`}/>
+                                                    <ErrorMessage name="confirmPassword" component="div" className="invalid-feedback"/>
+                                                </div>
+                                                <div className="pricing-table-cta mb-8">
+                                                    <button className="button button-primary button-shadow button-block submit-btn">Register now</button>
+                                                </div>
+                                            </Form>
+                                            )}
+                                        </Formik>
+                                        <div className="text-center mt-24">
+                                            <span className="text-xs">Have already an account ? <Link to={'/login'} className="login-link">Login here</Link></span>
                                         </div>
-                                        <div className="form-group">
-                                            <label className="form-label" htmlFor="user_email">Email</label>
-                                            <input type="text" id="user_email"
-                                                {...register('email')}
-                                                className={`form-control ${errors.email ? 'is-invalid' : ''}`}/>
-                                            <div className="invalid-feedback">{errors.email?.message}</div>
-                                        </div>
-                                        <div className="form-group">
-                                            <label className="form-label" htmlFor="user_password">Password</label>
-                                            <input type="password" id="user_password"
-                                                {...register('password')}
-                                                className={`form-control ${errors.password ? 'is-invalid' : ''}`}/>
-                                            <div className="invalid-feedback">{errors.password?.message}</div>
-                                        </div>
-                                        <div className="form-group">
-                                            <label className="form-label" htmlFor="user_password_confirmation">Confirm password</label>
-                                            <input type="password" id="user_password_confirmation"
-                                                {...register('confirmPassword')}
-                                                className={`form-control ${errors.confirmPassword ? 'is-invalid' : ''}`}/>
-                                            <div className="invalid-feedback">{errors.confirmPassword?.message}</div>
-                                        </div>
-                                        <div className="pricing-table-cta mb-8">
-                                            <button className="button button-primary button-shadow button-block submit-btn">
-                                                Register now
-                                            </button>
-                                        </div>
-                                    </form>
-                                    <div className="text-center mt-24">
-                                        <span className="text-xs">Have already an account ? <Link to={'/login'} className="login-link">Login here</Link></span>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    )
+            </section>
+        )
+    }
 }
+
 export default Register;
