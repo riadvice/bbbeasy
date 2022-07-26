7b21c4a8e42b8b26c1708fecccad7794de5998a3
chaima-tn <chaima.bouaziz@insat.u-carthage.tn>
2022-07-26 12:42:19 +0100

    Labels: Added `Actions\Labels\Delete->execute` API.

---
+++ hivelvet-backend/app/src/Actions/Labels/Delete.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Hivelvet open source platform - https://riadvice.tn/
+ *
+ * Copyright (c) 2022 RIADVICE SUARL and by respective authors (see below).
+ *
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation; either version 3.0 of the License, or (at your option) any later
+ * version.
+ *
+ * Hivelvet is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along
+ * with Hivelvet; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Actions\Labels;
+
+use Actions\Delete as DeleteAction;
+use Actions\RequirePrivilegeTrait;
+use Enum\ResponseCode;
+use Models\Label;
+/**
+ * Class Delete.
+ */
+class Delete extends DeleteAction {
+    use RequirePrivilegeTrait;  
+    public function execute($f3, $params): void
+    {
+        $label = new Label(); 
+        $labels_id = $params['id']; 
+        $label->load(['id = ?', $labels_id]);
+
+        if ($label->valid())
+        {
+            parent::execute($f3, $params);
+            $this->renderJson(['result' => 'success']);
+        }else
+        {
+            $this->renderJson([], ResponseCode::HTTP_NOT_FOUND);
+        }
+    }
+  
+}
