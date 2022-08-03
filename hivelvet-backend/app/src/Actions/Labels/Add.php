a0f816c4960294d69a1435ffcc30ed594c8ea9a0
chaima-tn <chaima.bouaziz@insat.u-carthage.tn>
2022-08-03 09:46:15 +0100

    Labels: run `hivelvet --fix`.

--- hivelvet-backend/app/src/Actions/Labels/Add.php
+++ hivelvet-backend/app/src/Actions/Labels/Add.php
@@ -39,8 +39,8 @@ class Add extends BaseAction
      */
     public function save($f3, $params): void
     {
-        $body        = $this->getDecodedBody();
-        $form        = $body['data'];
+        $body = $this->getDecodedBody();
+        $form = $body['data'];
 
         $dataChecker = new DataChecker();
         $dataChecker->verify($form['name'], Validator::notEmpty()->setName('name'));
