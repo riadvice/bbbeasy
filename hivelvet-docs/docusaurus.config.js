da310d4fc22d0bad232ca12aab0b1118d821c15d
Ghazi Triki <ghazi.triki@riadvice.tn>
2023-02-25 12:39:58 +0100

    Relaxed broken link when building documentation.

--- hivelvet-docs/docusaurus.config.js
+++ hivelvet-docs/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: 'RIADVICE', // Usually your GitHub org/user name.
   projectName: 'hivelvet', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
