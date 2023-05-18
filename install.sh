c77d73c07e51368a086a8f2f6914aaba3b786f2d
Ghazi Triki <ghazi.triki@riadvice.tn>
2023-05-18 10:01:50 +0100

    Fix install yarn in production.

--- install.sh
+++ install.sh
@@ -173,6 +173,7 @@ install_deps() {
   echo "Install yarn"
   curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+  apt update
   apt-get install -y yarn
   yarn set version berry
   npm install -g pm2
