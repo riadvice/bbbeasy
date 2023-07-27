8a64ed4afa5de81759c2c13bcb55566fd9c46615
aminecherif <amine.cherif@riadvice.tn>
2023-07-27 11:22:12 +0100

    Show the full name of the room.

--- bbbeasy-frontend/src/components/Rooms.tsx
+++ bbbeasy-frontend/src/components/Rooms.tsx
@@ -133,7 +133,7 @@ const RoomsCol: React.FC<RoomsColProps> = ({ index, room, editable, deleteClickH
                                 </Badge>
                             </Badge>
                             <Tooltip title={room.name} placement="top">
-                                <Title level={4} className="title-room">{room.name}</Title>
+                                <Title level={4} className="room-title">{room.name}</Title>
                             </Tooltip>
                         </Space>
                     </div>
